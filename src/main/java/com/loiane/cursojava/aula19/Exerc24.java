eb4046ca7a31034b214af4be722905fc542d49b0
Lucas Ferreira <lucasgabrielrf.contato@gmail.com>
2024-04-12 20:49:37 -0300

    updates dos exercicios

--- src/main/java/com/loiane/cursojava/aula19/Exerc24.java
+++ src/main/java/com/loiane/cursojava/aula19/Exerc24.java
@@ -2,9 +2,8 @@ package com.loiane.cursojava.aula19;
 
 public class Exerc24 {
     public static void main(String[] args) {
-        int[] a = new int[10];
-
         boolean flag = true;
+        int[] a = new int[10];
 
         for(int i = 0; i < a.length;i++){
             if(a[i] != a[a.length - 1 - i]){
