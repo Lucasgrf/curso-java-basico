4dfcc3db24723d539852be7eb953289e6f3d6e58
Lucas Ferreira <lucasgabrielrf.contato@gmail.com>
2024-04-12 20:47:05 -0300

    updates dos exercicios

--- src/main/java/com/loiane/cursojava/aula19/Exerc24.java
+++ src/main/java/com/loiane/cursojava/aula19/Exerc24.java
@@ -4,10 +4,25 @@ public class Exerc24 {
     public static void main(String[] args) {
         int[] a = new int[10];
 
-        for(int i = 0; i < a.length;i++){
-            if(a[i] == a[a.length - 1 - i]){
+        boolean flag = true;
 
+        for(int i = 0; i < a.length;i++){
+            if(a[i] != a[a.length - 1 - i]){
+                flag = false;
+                break;
             }
+        }
+        System.out.println("Vetor A: ");
+
+        for(int i = 0; i < a.length; i++){
+            System.out.println(a[i] + " ");
+        }
+        System.out.println();
+
+        if(flag){
+            System.out.println("Palidromo!");
+        }else{
+            System.out.println("Não é palidromo!");
         }
     }
 }
