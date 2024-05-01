6b9b93104dae08325caff460875eb3cd7a1e8f4d
Lucas Ferreira <lucasgabrielrf.contato@gmail.com>
2024-05-01 18:20:08 -0300

    novos exercicios sobre atributos de um objeto.

---
+++ src/main/java/com/loiane/cursojava/aula24/Lampada.java
@@ -0,0 +1,12 @@
+package com.loiane.cursojava.aula24;
+
+public class Lampada {
+    String tensao;
+    String marca;
+    int precoLampada;
+    int potencia;
+    String cor;
+    String tipoLuz;
+    int garantiaMeses;
+
+}
---
+++ src/main/java/com/loiane/cursojava/aula24/Livro.java
@@ -0,0 +1,10 @@
+package com.loiane.cursojava.aula24;
+
+public class Livro {
+    String tipo;
+    int paginas;
+    String modelo;
+    boolean imagens;
+
+
+}
