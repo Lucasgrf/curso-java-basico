0121d9d6edb14e0ccc9abcbd2819c27a2671a606
Lucas Ferreira <lucasgabrielrf.contato@gmail.com>
2024-05-14 17:00:25 -0300

    mais exercicios sobre metodos.

--- src/main/java/com/loiane/cursojava/aula24ate27/ContaCorrente.java
+++ src/main/java/com/loiane/cursojava/aula24ate27/ContaCorrente.java
@@ -1,7 +1,38 @@
 package com.loiane.cursojava.aula24ate27;
 
 public class ContaCorrente {
-    String status, nomePessoa;
-    int saldo, deposito, numeroConta, agencia;
-    boolean limitada;
+    int numeroConta, agencia;
+    double saldo, valorEspecialUsado, limiteEspecial;
+    boolean especial;
+
+    boolean realizarSaque(double quantiaSacar){
+        if(saldo >= quantiaSacar){
+            saldo -= quantiaSacar;
+            return true;
+        }else{
+            if(especial){
+                double limite = limiteEspecial + saldo;
+                if(limite >= quantiaSacar){
+                    saldo -= quantiaSacar;
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
+    }
+
+    void depositar(double valorDepositado){
+        saldo += valorDepositado;
+    }
+
+    void consultarSaldo(){
+        System.out.println("Saldo atual: " + saldo);
+    }
+
+    boolean verificarUsoChequeEspecial(){
+        return saldo < 0;
+    }
 }
--- src/main/java/com/loiane/cursojava/aula24ate27/Exerc05.java
+++ src/main/java/com/loiane/cursojava/aula24ate27/Exerc05.java
@@ -3,21 +3,39 @@ package com.loiane.cursojava.aula24ate27;
 public class Exerc05 {
     public static void main(String[] args) {
         ContaCorrente conta = new ContaCorrente();
-        conta.saldo = 0;
-        conta.deposito = 100;
+        conta.saldo = -10;
         conta.numeroConta = 223234;
-        conta.nomePessoa = "Lucas";
-        conta.status = "Especial";
-        conta.limitada = false;
+        conta.especial = true;
+        conta.limiteEspecial = 500;
         conta.agencia = 0001;
+        conta.valorEspecialUsado = 0;
 
-        System.out.println("Dados conta corrente: ");
+        /*System.out.println("Dados conta corrente: ");
         System.out.println("Número da conta: " + conta.numeroConta);
         System.out.println("Agência: " + conta.agencia);
-        System.out.println("Nome do dono da conta: " + conta.nomePessoa);
-        System.out.println("Status da conta: " + conta.status);
-        System.out.println("Conta limitada: " + conta.limitada);
-        System.out.println("Saldo da conta: " + conta.saldo);
-        System.out.println("Saldo pra deposito: " + conta.deposito);
+        System.out.println("Status da conta: " + conta.especial);
+        System.out.println("Limite Especial: " + conta.limiteEspecial);
+        System.out.println("Saldo da conta: " + conta.saldo);*/
+
+        boolean saqueEfetuado = conta.realizarSaque(500);
+        if(saqueEfetuado){
+            System.out.println("Saque efetuado com sucesso!");
+            conta.consultarSaldo();
+        }else{
+            System.out.println("Ímpossivel realizar saque. Sem limite especial suficiente!");
+        }
+
+        System.out.println("Depósito de 500 reais!");
+        conta.depositar(500);
+        conta.consultarSaldo();
+
+        conta.realizarSaque(600);
+        conta.consultarSaldo();
+
+        if(conta.verificarUsoChequeEspecial()){
+            System.out.println("Está usando cheque especial!");
+        }else{
+            System.out.println("Não está usando cheque especial!");
+        }
     }
 }
