9c78bf4c4f1c3343b7ba2ca12db339895d1c3eae
Lucas Ferreira <lucasgabrielrf.contato@gmail.com>
2024-05-19 16:49:38 -0300

    default, public e private nas classes do java.

---
+++ src/main/java/com/loiane/cursojava/aula31/TesteCarro.java
@@ -0,0 +1,10 @@
+package com.loiane.cursojava.aula31;
+
+public class TesteCarro {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.marca = "Fiat";
+
+        // carro.capCombustivel = 1; atributo private.
+    }
+}
