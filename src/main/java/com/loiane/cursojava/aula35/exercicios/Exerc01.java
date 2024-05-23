6086b4a78417c57a4b3fd9de135ceda275bdf2cb
Lucas Ferreira <lucasgabrielrf.contato@gmail.com>
2024-05-23 01:12:28 -0300

    exercicios sobre recursividade.

---
+++ src/main/java/com/loiane/cursojava/aula35/exercicios/Exerc01.java
@@ -0,0 +1,11 @@
+package com.loiane.cursojava.aula35.exercicios;
+
+public class Exerc01 {
+    public static void main(String[] args) {
+        //num = n-ésimo número da série de fibonnaci.
+        for(int i = 0; i < 20;i++){
+            System.out.print(Calculadora.fibonacci(i) + " ");
+        }
+    }
+
+}
---
+++ src/main/java/com/loiane/cursojava/aula35/exercicios/Exerc02.java
@@ -0,0 +1,8 @@
+package com.loiane.cursojava.aula35.exercicios;
+
+public class Exerc02 {
+    public static void main(String[] args) {
+        // num = número para fazer o somatorio. 5 = 5 + 4 + 3 + 2 + 1.
+        System.out.println(Calculadora.somatorio(5));
+    }
+}
