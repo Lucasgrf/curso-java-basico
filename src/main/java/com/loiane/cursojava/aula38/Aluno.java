4e4e0d8f4d9f764a8cf96d0daa807c6210d3bd40
Lucas Ferreira <lucasgabrielrf.contato@gmail.com>
2024-05-23 22:56:36 -0300

    aula 38.

---
+++ src/main/java/com/loiane/cursojava/aula38/Aluno.java
@@ -0,0 +1,38 @@
+package com.loiane.cursojava.aula38;
+
+public class Aluno extends Pessoa {
+    private String curso;
+    private double[] notas;
+
+    public Aluno() {
+        super(); // Faz referência aos atributos e métodos da superclass.
+    }
+
+    public Aluno(String nome, String endereco, String telefone, String curso) {
+        super(nome, endereco, telefone);
+        this.curso = curso;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public double calcularMedia(){
+        return 0;
+    }
+    public boolean verificarAprovado(){
+        return true;
+    }
+}
